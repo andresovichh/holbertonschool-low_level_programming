7f976ba92aba290ebae044ef5bd787777d5221ed
Andres Henderson <ahenderson.holberton@gmail.com>
2021-09-30 10:28:17 -0700

    6th

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - tata
+ * Return: always 0
+ */
+
+
+int main(void)
+{
+	int i;
+	double d;
+	char c;
+
+	prinf("The size of an int is: %\lu.\n", (unsigned long)sizeof(i));
+	prinf("The size of an double is: %\lu.\n", (unsigned long)sizeof(d));
+	prinf("The size of an c is: %\lu.\n", (unsigned long)sizeof(c));
+
+	return (0);
+}
