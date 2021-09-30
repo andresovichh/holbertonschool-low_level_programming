4df65532b93c043a836c7d854282368281d99a19
Andres Henderson <ahenderson.holberton@gmail.com>
2021-09-30 10:37:02 -0700

    6th

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,17 @@ int main(void)
 	int i;
 	double d;
 	char c;
+	long long int li;
+	long int lu;
+	float f;
+
+
 
 	printf("The size of an int is: %lu.\n", (unsigned long)sizeof(i));
 	printf("The size of an double is: %lu.\n", (unsigned long)sizeof(d));
 	printf("The size of an c is: %lu.\n", (unsigned long)sizeof(c));
-
+	printf("The size of an li is: %lu.\n", (unsigned long)sizeof(li));
+	printf("The size of an lu is: %lu.\n", (unsigned long)sizeof(lu));
+	printf("The size of an f is: %lu.\n", (unsigned long)sizeof(f));
 	return (0);
 }
