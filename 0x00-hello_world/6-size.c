f086785e0b78ae618496381381eb440b16b60a47
Andres Henderson <ahenderson.holberton@gmail.com>
2021-09-30 10:31:17 -0700

    6th

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,9 +11,9 @@ int main(void)
 	double d;
 	char c;
 
-	prinf("The size of an int is: %\lu.\n", (unsigned long)sizeof(i));
-	prinf("The size of an double is: %\lu.\n", (unsigned long)sizeof(d));
-	prinf("The size of an c is: %\lu.\n", (unsigned long)sizeof(c));
+	prinf("The size of an int is: %lu.\n", (unsigned long)sizeof(i));
+	prinf("The size of an double is: %lu.\n", (unsigned long)sizeof(d));
+	prinf("The size of an c is: %lu.\n", (unsigned long)sizeof(c));
 
 	return (0);
 }
