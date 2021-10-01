d1c100778372efbafba9e6e07d29e6056a9ba952
Andres Henderson <ahenderson.holberton@gmail.com>
2021-10-01 11:40:52 -0700

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Tdasdasdasd
+ * Return: always zero
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = '0'; ch <= '8'; ch++)
+	{
+		putchar(ch);
+		putchar(',');
+	}
+	putchar('9');
+		putchar('\n');
+	return (0);
+}
