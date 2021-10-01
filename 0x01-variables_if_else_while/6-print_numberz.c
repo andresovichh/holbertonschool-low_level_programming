d62d4b3ed3ffd4f6080688fc2f900d373beee117
Andres Henderson <ahenderson.holberton@gmail.com>
2021-10-01 10:56:39 -0700

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Tdasdasdasd
+ * Return: always zero
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 48; ch <= 57; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
