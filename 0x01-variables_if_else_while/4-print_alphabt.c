b740597db41cd31c14fe7c4228427c8c539f09fd
Andres Henderson <ahenderson.holberton@gmail.com>
2021-10-01 10:02:20 -0700

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Tdasdasdasd
+ * Return: always zero
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z' && ch != 'q' && 'e'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
