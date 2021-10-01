334906317be996c56d79966d6513281e98960046
Andres Henderson <ahenderson.holberton@gmail.com>
2021-10-01 12:00:23 -0700

    9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,13 +9,17 @@ int main(void)
 {
 	int ch;
 
-	for (ch = 48; ch <= 56; ch++)
+	for (ch = 48; ch <= 57; ch++)
 	{
 		putchar(ch);
-		putchar(32);
+		if (ch < 56)
+		{
 		putchar(44);
+		putchar(32);
+		}
 	}
-	putchar(57);
+
 		putchar('\n');
+
 	return (0);
 }
