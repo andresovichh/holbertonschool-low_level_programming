7fdfbeaeeefc5da54b1b7fb88102d915ccb36b59
Andres Henderson <ahenderson.holberton@gmail.com>
2021-10-01 11:25:18 -0700

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Tdasdasdasd
+ * Return: always zero
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
