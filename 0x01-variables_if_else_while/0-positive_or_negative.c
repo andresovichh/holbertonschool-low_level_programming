56847333e78a18dbba1388d8e3cf568454bbd81d
Andres Henderson <ahenderson.holberton@gmail.com>
2021-10-01 09:05:08 -0700

    00

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,17 +4,17 @@
 /* main - Tdasdasdasd
  * Return: always zero
  */
-int main(void)
+int main (void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if (n==0)printf("%d is zero",n);
-	else if(n< 0)printf("%d is negative", n);
-	else printf("%d is negative \n", n);
-	
-	
-	return (0);
+if (n == 0)
+printf("%d is zero", n);
+else if (n < 0)
+printf("%d is negative", n);
+else printf("%d is positive \n", n);
+return (0);
 }
---
+++ 0x01-variables_if_else_while/modelo.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* main - Tdasdasdasd
+ * Return: always zero
+ */
+int main(void)
+{
+ 	
+	return (0);
+}
