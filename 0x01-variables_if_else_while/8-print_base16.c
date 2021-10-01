ba3f6aa57a525765dc79e909122713718a3467e1
Andres Henderson <ahenderson.holberton@gmail.com>
2021-10-01 11:33:39 -0700

    8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Tdasdasdasd
+ * Return: always zero
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 48; ch <= 57; ch++)
+		putchar(ch);
+	for (ch = 97; ch <= 102; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
