3edf13993fed77dcebb762b650cb953f311425b3
Andres Henderson <ahenderson.holberton@gmail.com>
2021-10-01 09:59:13 -0700

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Tdasdasdasd
+ * Return: always zero
+ */
+int main(void)
+{
+	char ch;
+	char uc;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (uc = 'A'; uc <= 'Z'; uc++)
+		putchar(uc);
+	putchar('\n');
+	return (0);
+}
