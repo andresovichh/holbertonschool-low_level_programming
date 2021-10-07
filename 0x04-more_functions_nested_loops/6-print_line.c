984273fb3f78a6afedecf396e9ae27d3eb287e6c
Andres Henderson <ahenderson.holberton@gmail.com>
2021-10-07 11:08:25 -0700

    6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - easdasd
+ *
+ * Return: Always 0
+ *
+ * @n: das
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+		_putchar('_');
+		}
+	}
+	_putchar('_');
+
+}
