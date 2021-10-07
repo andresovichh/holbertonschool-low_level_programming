b67f465379fab21f0bce8673085eb2206ff95003
Andres Henderson <ahenderson.holberton@gmail.com>
2021-10-07 11:52:56 -0700

    7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,6 +9,9 @@ void print_diagonal(int n)
 	int i;
 	int j;
 
+	if (n > 0)
+	{
+
 	for (i = 1; i <= n; i++)
 	{
 		for (j = 1; j <= n; j++)
@@ -17,8 +20,9 @@ void print_diagonal(int n)
 			else
 				_putchar(' ');
 		_putchar('\n');
-
-
 	}
+	}
+	else
+		_putchar('\n');
 
 }
