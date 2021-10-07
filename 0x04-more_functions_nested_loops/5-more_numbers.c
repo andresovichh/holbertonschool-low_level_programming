4bf0c8411e4b9d0af02826c88ac5ee80354fc792
Andres Henderson <ahenderson.holberton@gmail.com>
2021-10-07 10:33:10 -0700

    5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * more_numbers - easdasd
+ *
+ * Return: Always 0
+ *
+ *
+ */
+
+void more_numbers(void)
+{
+	int i;
+	char j;
+		for (i = 0; i <= 9; i++)
+		{
+			for (j = 0; j <= 14; j++)
+			{
+				if (j >= 10)
+				{
+					_putchar((j / 10) + 48);
+
+				}
+				{
+					_putchar((j % 10) + 48);
+
+				}
+
+
+			}
+
+		}
+		_putchar('\n');
+
+
+}
