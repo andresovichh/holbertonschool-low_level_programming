5749659993a09172b04811946cc67dddc47d92a1
Andres Henderson <ahenderson.holberton@gmail.com>
2021-10-07 06:48:06 -0700

    2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,17 +12,20 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && (b > c || a > c))
+	if (a > b)
 	{
-	largest = a;
-	}
-	else if (b > a && (a > c || b > c))
-	{
-	largest = b;
+		if (a > c)
+			largest = a;
+		else
+			largest = c;
 	}
 	else
 	{
-		largest = c;
+		if (b > c)
+			largest = b;
+		else
+			largest = c;
 	}
 	return (largest);
+
 }
