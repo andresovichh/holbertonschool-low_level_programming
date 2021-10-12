3a8df9bb6222449b0b75c76c4c5849757beb932c
Andres Henderson <ahenderson.holberton@gmail.com>
2021-10-12 10:40:28 -0700

    3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _puts - junsjnds
+ *
+ *
+ *
+ * @str: minnas
+ *
+ */
+
+
+void _puts(char *str)
+{
+	int c;
+
+	for (c = 0; str[c] != '\n'; c++)
+	{
+		_putchar(str[c]);
+	}
+	_putchar('\n');
+
+}
