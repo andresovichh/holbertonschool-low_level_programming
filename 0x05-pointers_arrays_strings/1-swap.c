3d584fd27820a25635278d642dbc7d2822c1f194
Andres Henderson <ahenderson.holberton@gmail.com>
2021-10-12 08:58:30 -0700

    1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * swap_int - dasdasd
+ *
+ * @tmp,a,b: dasdads
+ *
+ * Return: Always 0
+ * @b: dasd
+ *
+ *
+ */
+
+void swap_int(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+
+}
