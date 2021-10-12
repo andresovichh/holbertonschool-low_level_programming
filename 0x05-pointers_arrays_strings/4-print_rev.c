5be800af733b79b033f148eb5041bd898328f898
Andres Henderson <ahenderson.holberton@gmail.com>
2021-10-12 11:23:45 -0700

    4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev -  suun jnas sdjkn
+ *
+ * @s: jnmnxm
+ *
+ *
+ *
+ */
+
+void print_rev(char *s)
+{
+	int c = 0;
+
+
+	for (c = 0; s[c] != '\0'; c++)
+	{
+		_putchar(~s[c]);
+	}
+	_putchar('\n');
+
+
+}
