a5ddfdbac693d99fb15556bd88a1e3b604f4d753
Andres Henderson <ahenderson.holberton@gmail.com>
2021-10-12 09:26:36 -0700

    2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strlen - dasdas
+ *
+ *
+ * @s: dafddsfsd
+ * Return: Always total
+ *
+ */
+
+int _strlen(char *s)
+{
+	int total = 0;
+
+	for (total = 0; total <= *s; total++)
+	{
+		total = total + 1;
+	}
+	return (total);
+
+
+
+
+}
