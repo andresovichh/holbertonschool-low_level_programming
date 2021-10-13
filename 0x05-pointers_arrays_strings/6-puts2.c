1d75445eefb7f6b63bad0d91c838e998fbd9a5be
Andres Henderson <ahenderson.holberton@gmail.com>
2021-10-12 19:21:05 -0700

    6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -17,22 +17,11 @@ void puts2(char *str)
 		;
 	l = i;
 
-	for (i = 0; i <= l; i++)
+	for (i = 0; i <= l; i += 2)
 	{
-		if (i == 0)
-		{
-			_putchar(str[i]);
-		}
-		else if (i == 1)
-		{
-		}
-		else if (i % 2)
-		{
-			_putchar(str[i]);
-		}
-		else
-		{
-		}
+		_putchar(str[i]);
+
+
 
 	}
 	 _putchar('\n');
