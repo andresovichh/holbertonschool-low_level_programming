6b9b37c5b964d2f9305995968b916b6db6a181a0
Andres Henderson <ahenderson.holberton@gmail.com>
2021-10-13 07:55:31 -0700

    8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,13 +12,12 @@
 
 void print_array(int *a, int n)
 {
-	int i, l;
+	int i;
 
 	for (n = 0; a[n] != '\0'; n++)
 		;
-	l = n;
 
-	for (i = 0; i < l; i++)
+	for (i = 0; i < n; i++)
 	{
 		printf("%d, ", a[i]);
 
