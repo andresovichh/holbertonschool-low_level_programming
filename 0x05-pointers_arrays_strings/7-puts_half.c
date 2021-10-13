74a061c9331c1da8051f7d31575381933c49c33a
Andres Henderson <ahenderson.holberton@gmail.com>
2021-10-13 05:21:06 -0700

    7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,22 +11,15 @@
 
 void puts_half(char *str)
 {
-	int i, l;
+	int i, l = 0;
 
 	for (i = 0; str[i] != '\0'; i++)
 		;
 	l = i;
 
-	for (i = 0; i <= l; i++)
+	for (i = l / 2; str[i] != '\0'; i++)
 	{
-		if (l % 2 == 0 && i >= l / 2)
-		{
-			_putchar(str[i]);
-		}
-		else if (l % 2 != 0 && i >= l / 2)
-		{
-			_putchar(str[(i - 1) / 2]);
-		}
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
