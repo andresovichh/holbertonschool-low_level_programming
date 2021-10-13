218987ef6fd9f80ed1f111aaac51691f1b15e271
Andres Henderson <ahenderson.holberton@gmail.com>
2021-10-13 07:48:30 -0700

    8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - reverses a string
+ *
+ * @a: length of string
+ * @n: dasdkijmkmsakm
+ * Return: Always 0
+ *
+ */
+
+void print_array(int *a, int n)
+{
+	int i, l;
+
+	for (n = 0; a[n] != '\0'; n++)
+		;
+	l = n;
+
+	for (i = 0; i <= l; i++)
+	{
+		printf("%d", a[i]);
+		printf("\n");
+
+	}
+	printf("\n");
+}
