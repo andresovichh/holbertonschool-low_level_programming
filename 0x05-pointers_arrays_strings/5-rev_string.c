042e1b15c662db492b8b2410cc5c5faf946bbdbf
Andres Henderson <ahenderson.holberton@gmail.com>
2021-10-12 18:34:55 -0700

    5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,34 +11,19 @@
 
 void rev_string(char *s)
 {
-	int i, l, j = 0;
-	char tmp;
-
-	/**
-	* for (i = 0; s[i] != '\0'; i++)
-	* {
-	* l = i++;
-	* }
-	*/
+	int i, j, l = 0;
+	int tmp;
 
 
 	for (i = 0; s[i] != '\0'; i++)
+		;
+
+	l = i;
+
+	for (j = 0; j < (l / 2); j++)
 	{
-		l = i++;
-	}
-
-
-	for (j = 0; j <= (l / 2); j++)
-	{
-
 		tmp = s[j];
-		s[j] = s[l - j - 1];
-
-		s[l - j - 1] = tmp;
+		s[j] = s[l - 1 - j];
+		s[l - 1 - j] = tmp;
 	}
-
-
-
-
-
 }
