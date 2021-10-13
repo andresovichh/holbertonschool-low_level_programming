532234675bc7b4f6ffd3bf67d00a8b5942beadcf
Andres Henderson <ahenderson.holberton@gmail.com>
2021-10-12 19:08:57 -0700

    6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * puts2 - reverses a string
+ *
+ * @str: length of string
+ *
+ * Return: Always 0
+ *
+ */
+
+void puts2(char *str)
+{
+	int i, l;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	l = i;
+
+	for (i = 0; i <= l; i++)
+	{
+		if (i % 2)
+		{
+		}
+		else
+		{
+			_putchar(str[i]);
+		}
+
+	}
+	 _putchar('\n');
+
+}
