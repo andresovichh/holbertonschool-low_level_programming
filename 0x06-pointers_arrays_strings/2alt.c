31d43189bcc3b57401901f5c0627a1eee8f2a7a8
Andres Henderson <ahenderson.holberton@gmail.com>
2021-10-15 05:26:24 -0700

    3 Checks for ascii difference

--- 0x06-pointers_arrays_strings/2alt.c
+++ 0x06-pointers_arrays_strings/2alt.c
@@ -12,14 +12,13 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
+	int i;
 
-	for (i = 0; src[i] != '\0' && i <= n; i++)
-	{
+	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
-	}
-	dest[i] = '\0';
-return (dest);
-}
+	for ( ; i < n; i++)
+		dest[i] = '\0';
 
+	return (dest);
+}
 /** Check if the output is the right one */
---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compares two strings
+ *
+ * @s1: a string
+ * @s2: Another string
+ *
+ * Return: multiple returns
+ *
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		if (s1[i] == s2[i])
+		{
+			return (0);
+		}
+		else if (s1[i] > s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		else if (s1[i] < s2[i])
+		{
+			return (-(s2[i] - s1[i]));
+		}
+	}
+	return (0);
+}
