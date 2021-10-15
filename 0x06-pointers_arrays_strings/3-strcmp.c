d2291bc1c9bdb1319192d7f3381b307386b5dc73
Andres Henderson <ahenderson.holberton@gmail.com>
2021-10-15 10:36:55 -0700

    3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,23 +12,12 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+	int i = 0;
+	int val;
 
-	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
-	{
-		if (s1[i] == s2[i])
-		{
-			return (0);
-		}
-		else if (s1[i] > s2[i])
-		{
-			return (s1[i] - s2[i]);
-		}
-		else if (s1[i] < s2[i])
-		{
-			return (s1[i] - s2[i]);
-		}
+	while (s1[i] != 0 && s2[i] != 0 && s1[i] == s2[i])
 		i++;
-	}
-	return (0);
+val = s1[i] - s2[i];
+return (val);
+
 }
