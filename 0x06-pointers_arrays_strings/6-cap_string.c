d8ed717365eebe31418341219fe67fae12b95d55
Andres Henderson <ahenderson.holberton@gmail.com>
2021-10-15 12:14:48 -0700

    6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,9 +11,9 @@
 
 char *cap_string(char *s)
 {
-	int i = 0;
+	int i = 1;
 
-	for (i = 0; s[i] != 0; i++)
+	for (i = 1; s[i] != '\0'; i++)
 	{
 		if ((s[i - 1] == ','
 				|| s[i - 1] == ';'
@@ -28,11 +28,12 @@ char *cap_string(char *s)
 				|| s[i - 1] == '\n'
 				|| s[i - 1] == 32
 				|| s[i - 1] == '}')
-				&& (s[i] < 123 && s[i] > 96))
+				&& (s[i] < 123 && s[i] > 96) && i > 1)
 		{
+			s[0] = s[0] - 32;
 			s[i] = s[i] - 32;
 		}
 	}
 
-return (0);
+return (s);
 }
