2cc1123c8d201431baf3e652451db3d18ed3ebb4
Andres Henderson <ahenderson.holberton@gmail.com>
2021-10-18 09:47:04 -0700

    2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -14,8 +14,13 @@ char *_strchr(char *s, char c)
 	int i = 0;
 
 
-	for (i = 0; s[i] != c && s[i] != 0; i++)
+	for (i = 0 ; s[i] != c && s[i] != 0; i++)
 	{
 	}
-	return (&s[i]);
+	if (s[i] == c)
+	{
+		return (&s[i]);
+	}
+	else
+		return (0);
 }
