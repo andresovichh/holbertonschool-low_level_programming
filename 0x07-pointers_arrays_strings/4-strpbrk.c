39531b2179bb63d61a8419bec7ea0f99c2e64b7b
Andres Henderson <ahenderson.holberton@gmail.com>
2021-10-18 14:12:04 -0700

    4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -15,16 +15,16 @@ char *_strpbrk(char *s, char *accept)
 
 	for (i = 0; s[i] != 0; i++)
 	{
-		if (s[i] == accept[j])
+		for (j = 0; accept[j] != 0; j++)
+	{
+		if (*s == accept[j])
 		{
-			s[i] = accept[j];
-			j++;
+			return (s);
 		}
-			else
-			{
-			}
 
-		}
-return (s);
+	}
+	s++;
+	}
+return (0);
 }
 
