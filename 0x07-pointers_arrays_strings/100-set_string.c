10d675665d02387aeebb617b773ee5332bf3c07d
Andres Henderson <ahenderson.holberton@gmail.com>
2021-10-18 19:13:45 -0700

    100

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * set_string - Finds a character in a string
+ * @s: Name of string
+ * @to: whatever
+ * Return: multiple returns
+ *
+ */
+
+
+void set_string(char **s, char *to)
+{
+*s = to;
+}
