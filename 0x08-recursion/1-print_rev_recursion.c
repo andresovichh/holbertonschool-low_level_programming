2dde5ddea1fd8bebb88de01a62138b5553413a12
Andres Henderson <ahenderson.holberton@gmail.com>
2021-10-20 07:58:53 -0700

    1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -9,11 +9,12 @@
 
 void _print_rev_recursion(char *s)
 {
-	if (*s == 0)
+	if (*s != 0)
 	{
-		_putchar(10);
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 		return;
 	}
-	_print_rev_recursion(s + 1);
-	_putchar(*s);
+	_putchar(10);
+
 }
