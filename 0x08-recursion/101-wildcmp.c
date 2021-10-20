b490fe05df8d72e14b07cb57e937090bb7ae7a8f
Andres Henderson <ahenderson.holberton@gmail.com>
2021-10-20 12:33:22 -0700

    101

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * wildcmp- Pal ind
+ *
+ * @s1: number
+ * @s2: another
+ *
+ * Return: Always 0
+ *
+ */
+
+
+int wildcmp(char *s1, char *s2)
+{
+
+	return (*s1 + *s2);
+}
