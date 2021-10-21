d533dd89d555e9ee8d443256cdd5df470c53e2b1
Andres Henderson <ahenderson.holberton@gmail.com>
2021-10-21 09:01:36 -0700

    5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,25 +1,41 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - Square of a number
- *
- * @n: an int
- *
+ * aux - Auxiliary function
+ * @n: n
+ * @i: i
  * Return: Awlays 0
+ */
+
+
+
+int aux(int n, int i)
+{
+	if (i * i == n)
+		return (i);
+	else if (i * i > n)
+		return (-1);
+	else
+		return (aux(n, i + 1));
+}
+
+
+/**
+ * _sqrt_recursion - dasd
+ * @n: das
+ *
+ * Return: always 0
  */
 
 int _sqrt_recursion(int n)
 {
-	int *n;
-
-	if ((n / 2) * (n / 2)  == n)
-	{
-		return (n / 2);
-	}
+	if (n < 0)
+	return (-1);
+	else if (n == 0)
+	return (0);
 	else
-	{
-		++n;
-		_sqrt_recursion(n - 1);
-	}
-return (n);
+	return (aux(n, 1));
+
+
+
 }
