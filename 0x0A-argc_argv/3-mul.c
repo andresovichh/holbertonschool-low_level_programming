67b542facb927262d32dd1c0c2767a7d8743bd04
Andres Henderson <ahenderson.holberton@gmail.com>
2021-10-22 09:29:03 -0700

    3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - mdasin
+ * @argc: das
+ *
+ * @argv: kmkmsd
+ *
+ * Return: Always 0
+ *
+ */
+
+int main(int argc, char **argv)
+{
+	if (argc < 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		printf("%d\n", (atoi(argv[1]) * atoi(argv[2])));
+		return (0);
+	}
+}
