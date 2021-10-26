618d541aae30393a13c415d6fc2dc703c627011e
Andres Henderson <ahenderson.holberton@gmail.com>
2021-10-26 15:29:43 -0700

    100

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * argstostr - returns pointer to 2 dim array of int
+ *
+ * @ac: width of array
+ * @av: height of array
+ * Return: Always 0
+ */
+
+
+char *argstostr(int ac, char **av)
+{
+	if (ac == 0)
+		return (0);
+	if (*av == 0)
+	return (0);
+return (0);
+}
