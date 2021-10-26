ebd8c8dd53387092ba9bc7a7296f191a14631818
Andres Henderson <ahenderson.holberton@gmail.com>
2021-10-26 15:32:27 -0700

    101

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * strtow - returns pointer to 2 dim array of int
+ *
+ * @str: width of array
+ * Return: Always 0
+ */
+
+
+char **strtow(char *str)
+{
+	if (str != 0)
+		return (0);
+return (0);
+}
