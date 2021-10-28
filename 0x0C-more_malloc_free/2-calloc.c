9bbf1cab568108ecf6ed6f6ff27f7e1987572490
Andres Henderson <ahenderson.holberton@gmail.com>
2021-10-28 11:17:15 -0700

    2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -20,7 +20,6 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	arr = malloc(nmemb * size);
 	if (arr == NULL)
 		return (NULL);
-	if (arr != NULL)
 	for (i = 0; i < nmemb * size; i++)
 		arr[i] = 0;
 return (arr);
