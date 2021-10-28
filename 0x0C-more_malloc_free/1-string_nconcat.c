a6fce5a2468e3ee33f684442cbfb5a8777ec9a49
Andres Henderson <ahenderson.holberton@gmail.com>
2021-10-28 09:39:43 -0700

    1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,54 +2,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- * string_nconcat - String concatenations
- *
- * @s1: string 1.
- * @s2: string 2
- *
- * @n: unsigned int
- *
- * Return: Always 0
- */
 
+/**
+ * string_nconcat - Concatenates two strings
+ *
+ * @s1: a string
+ * @s2: another
+ * @n: length
+ * Return: Always 0
+ *
+ */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int i, j, k, l = 0;
-
 	char *new_s;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	for (i = 0; s1[i] != 0; i++) /** Measure string*/
-		;
-	for (j = 0; s2[j] != 0; j++) /** Measure other string*/
-		;
-	if (n < j)
-	{
-		new_s = malloc(sizeof(char) * (i + n + 1));
-		if (new_s == (NULL))
-			return (NULL);
-	}
-	else
-	{
-		new_s = malloc(sizeof(char) * (i + j + 1));
-		if (new_s == NULL)
-			return (NULL);
-	}
 
+	/** Measure both strings*/
+	for (i = 0; s1[i] != 0; i++)
+	{
+	}
+	for (j = 0; s2[j] != 0; j++)
+	{
+	}
+	if (n >= j)
+		n = j;
+	/** Allocate memory for both strings*/
+	new_s = malloc(sizeof(char) * (i + j + 1));
+
+	if (new_s == NULL)
+		return (NULL);
+	/** Concatenate strings*/
 	for (k = 0; k <= i; k++)
 	{
 		new_s[k] = s1[k];
 	}
-	while (l <= n)
+	for (k = i; k <= (i + j); k++)
 	{
-		new_s[k + 1] = s2[l++];
+		new_s[k] = s2[l];
+		l++;
 	}
-	new_s[k] = 0;
 
 	return (new_s);
 }
