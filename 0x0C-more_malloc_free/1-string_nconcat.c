b6962e92e202576651c1a153b424edaef329567d
Andres Henderson <ahenderson.holberton@gmail.com>
2021-10-28 08:48:20 -0700

    1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,7 +16,7 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j = 0;
+	unsigned int i, j, k, l = 0;
 
 	char *new_s;
 
@@ -24,31 +24,34 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	while (s2)
-		i++;
-	while (s1)
-		j++;
+	for (i = 0; s2[i] != 0; i++) /** Measure string*/
+		;
+	for (j = 0; s1[j] != 0; j++) /** Measure other string*/
+		;
 	if (n >= i)
 	{
-		new_s = malloc(sizeof(char) * (i + j));
+		new_s = malloc(sizeof(char) * (i + j + 1));
 		if (new_s == NULL)
 		{
 			return (NULL);
-			free(new_s);
 		}
-		new_s = str_concat(s1, s2);
 	return (new_s);
 	}
 	else
 	{
-		new_s = malloc(sizeof(char) * (i + n));
+		new_s = malloc(sizeof(char) * (i + n + 1));
 		if (new_s == NULL)
 		{
-			return (NULL);
-			free(new_s);
 		}
-		new_s = str_concat(s1, s2);
 		return (new_s);
 	}
-
+	for (k = 0; s1[k] != 0; k++)
+		new_s[k] = s1[k];
+	for (l = 0; s2[l] != 0; l++)
+	{
+		new_s[k + 1] = s2[l];
+		k++;
+	}
+	new_s[i + j] = 0;
+	return (new_s);
 }
