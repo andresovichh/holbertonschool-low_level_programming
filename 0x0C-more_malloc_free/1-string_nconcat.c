7de58f3491d9fba0bcddd3051f3fd7c47807ddec
Andres Henderson <ahenderson.holberton@gmail.com>
2021-10-28 10:22:18 -0700

    1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,12 +17,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int i, j, k, l = 0;
 	char *new_s;
-	/**
-	*if (s1 == NULL)
-	*	s1 = "";
-	*if (s2 == NULL)
-	*	s2 = "";
-	*/
+	
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 	/** Measure both strings*/
 	for (i = 0; s1[i] != 0; i++)
 	{
@@ -31,7 +31,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 	}
 	if (n >= j)
-		new_s = malloc(sizeof(char) * (i + n + 1));
+		new_s = malloc(sizeof(char) * (i + j + 1));
 	else
 		new_s = malloc(sizeof(char) * (i + n + 1));
 
@@ -47,7 +47,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		new_s[k] = s2[l];
 		l++;
 	}
-	for (k = i; k <= (i + n) && n >= j; k++)
+	for (k = i; s2[k] != 0 && n >= j; k++)
 	{
 		new_s[k] = s2[l];
 		l++;
