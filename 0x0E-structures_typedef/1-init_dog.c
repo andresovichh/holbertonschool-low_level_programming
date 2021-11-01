efc4efc07ba9a66bde313e548b24f23a1b097407
Andres Henderson <ahenderson.holberton@gmail.com>
2021-11-01 13:27:33 -0700

    0

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,4 +2,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * init_dog - Initializing a variable of type struct dog
+ *
+ * @d: pointer
+ * @name: A name
+ * @age: The age
+ * @owner: The name of the owner
+ *
+ */
+
+
 
