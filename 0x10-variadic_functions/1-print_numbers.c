def604894a59e0994d5f65ff6e0ff2786cc7df4a
Andres Henderson <ahenderson.holberton@gmail.com>
2021-11-04 06:39:18 -0700

    1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - Prints n separated by separator
+ * @separator: Separator
+ * @n: number of args
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap; /** Declare variable of type va_list*/
+	unsigned int i;
+
+	va_start(ap, n); /** Call va_start macro with var va_list and las param"*/
+
+	for (i = 0; i < n; i++)/** Run through the list*/
+	{
+		printf("%d", va_arg(ap, unsigned int)); /** Print each iteration*/
+		if (separator != NULL)/** Print separator only if exists*/
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(ap); /** Free the list*/
+}
