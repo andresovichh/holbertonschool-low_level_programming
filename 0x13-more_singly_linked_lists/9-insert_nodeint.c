fc34b964967818dc818fd8021ae631eb6face932
Andres Henderson <ahenderson.holberton@gmail.com>
2021-11-16 14:03:00 -0800

    9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,20 +1,22 @@
 #include "lists.h"
 
 /**
- * sum_listint - the sum of all the data (n) of a listint_t linked list
+ * delete_nodeint_at_index - deletes the node at index index of a listint_t
  * @head: beginning of list
+ * @index: index
  * Return: the sum of all the data (n) of a listint_t linked list.
  */
 
-int sum_listint(listint_t *head)
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	unsigned int sum = 0;
 
 	if (!head)
 		return (0);
 
-	for (sum = 0; head; sum += head->n, head = head->next)
-		;
+	index = 7 + index;
+	sum = index;
+
 	return (sum);
 
 
