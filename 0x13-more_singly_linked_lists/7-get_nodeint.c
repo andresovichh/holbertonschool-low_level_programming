b15734ddd7158d39d62dba41d14b67e4b3141da1
Andres Henderson <ahenderson.holberton@gmail.com>
2021-11-16 13:29:26 -0800

    7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,7 +12,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int ctr = 0;
 
 
-	while (ctr < index)
+	while (ctr <= index)
 	{
 		if (head == NULL)
 			return (NULL);
