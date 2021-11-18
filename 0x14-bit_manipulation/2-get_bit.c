d697d38bd2fe76dcf097da44aa6aeabcdab3dcee
Andres Henderson <ahenderson.holberton@gmail.com>
2021-11-18 11:49:17 -0800

    2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -13,7 +13,7 @@ int get_bit(unsigned long int n, unsigned int index)
 	if (n)
 		return (n & 1);
 
-	else if (!n || index > (sizeof(unsigned int) * 8))
+	if (index > (sizeof(unsigned int) * 8))
 		return (-1);
 return (0);
 }
