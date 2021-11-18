45982f82577becca2d7c85199a848cbee2d0dcac
Andres Henderson <ahenderson.holberton@gmail.com>
2021-11-18 10:46:40 -0800

    1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,15 +6,11 @@
  */
 void print_binary(unsigned long int n)
 {
-	if (n == 0)
+	if (n <= 1)
 	{
-		_putchar('0');
+		_putchar(n + '0');
 		return;
 	}
-	if (n == 1)
-	{
-		_putchar('1');
-		return;
-	}
-
+	print_binary(n >> 1);
+	_putchar((n & 1) + '0');
 }
