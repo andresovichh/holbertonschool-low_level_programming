376160ee1ec8453d1184c745ef8576ab66df66b0
Andres Henderson <ahenderson.holberton@gmail.com>
2021-11-18 11:18:03 -0800

    1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -11,6 +11,12 @@ void print_binary(unsigned long int n)
 		_putchar(n + '0');
 		return;
 	}
+	/**
+	 * When you perform right shifting operation
+	 * you are effectively dividing by 2 ^righthand operator.
+	 * In this case, that means you are dividing by 2, because
+	 * 2^1 = 2.
+	 */
 	print_binary(n >> 1);
 	_putchar((n & 1) + '0');
 }
