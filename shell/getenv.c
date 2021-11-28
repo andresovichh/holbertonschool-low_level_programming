9a86bc81488a648d8e9ab9c59df024c373d09bff
Andres Henderson <ahenderson.holberton@gmail.com>
2021-11-28 04:15:07 -0800

    _getenv

---
+++ shell/getenv.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * _getenv - gets the environment variable "name"
+ * @name: name of the variable
+ * Return: pointer to corresponding value string
+ */
+
+char *_getenv(const char *name)
+{
+
+
+	unsigned int i = 0, len = 0;
+	/** first check wether name exists*/
+	if (!name)
+	{
+		printf("NO NAME");
+		return (NULL);
+	}
+	/** then, measure length of name*/
+	len = strlen(name);
+
+
+	/** then, walk the environ variables*/
+	while (environ[i])
+	{
+		/** then check if environ is equal to name variable*/
+		if (strncmp(environ[i], name, len) == 0)
+		{
+			printf("%s\n", environ[i]);
+			return (environ[i]);
+		}
+		i++;
+	}
+	return (0);
+
+}
+int main(void)
+{
+	_getenv("LANG");
+	_getenv("PATH");
+	return (0);
+}
+
--- shell/main.h
+++ shell/main.h
@@ -6,7 +6,16 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <errno.h>
+char *_getenv(const char *name);
 extern char **environ;
 typedef struct tok 
 {
