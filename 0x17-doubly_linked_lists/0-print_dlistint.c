cc7384b826c330278ae1165c2b5a7a727053f7ef
andresovichh <ahenderson.holberton@gmail.com>
2022-01-08 15:53:06 -0300

    test

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,18 +1,19 @@
 #include "lists.h"
 /**
  * print_dlistint - prints a dl list
- * @h: begining of list 
- * Return: Always 0 
+ * @h: begining of list
+ * Return: Always 0
  */
+
 size_t print_dlistint(const dlistint_t *h)
 {
-    const dlistint_t *i = h;
-    size_t nodes = 0;
+const dlistint_t *i = h;
+size_t nodes = 0;
 
-    for(; i != NULL; i = i->next)
-    {
-        printf("%d\n", i->n);
-        nodes++;
-    };
+for (; i != NULL; i = i->next)
+{
+printf("%d\n", i->n);
+nodes++;
+}
 return (nodes);
 }
