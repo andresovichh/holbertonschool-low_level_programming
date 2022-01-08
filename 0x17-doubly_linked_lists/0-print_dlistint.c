d586c016445c91c45bf28b3b7de3b20a29b2e86d
andresovichh <ahenderson.holberton@gmail.com>
2022-01-08 14:54:18 -0300

    test

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,10 +10,12 @@
 size_t print_dlistint(const dlistint_t *h)
 {
     const dlistint_t *i = h;
+    size_t nodes = 0;
 
     for(; i != NULL; i = i->next)
     {
         printf("%d\n", i->n);
+        nodes++;
     };
-return (0);
+return (nodes);
 }
