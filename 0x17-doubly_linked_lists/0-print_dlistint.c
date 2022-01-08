e63711b66b0420855b73cb0d4b9b3ff4cdb0bd14
andresovichh <ahenderson.holberton@gmail.com>
2022-01-08 14:29:07 -0300

    test

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+
+/**
+ * print_dlistint - prints a dl list
+ * @h: begining of list 
+ * Return: Always 0 
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+
+        i++;
+    };
+
+}
