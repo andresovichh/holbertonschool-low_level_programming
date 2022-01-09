8675283568cd8e8ad38c425df7937f8f64d75fe9
andresovichh <ahenderson.holberton@gmail.com>
2022-01-09 10:02:52 -0300

    test

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,9 +1,10 @@
 #include "lists.h"
 
 /**
- *add_dnodeint_end - 
+ *add_dnodeint_end - Adds node at the end 
  *@head: ptr to head
  *@n: data for the node
+ * Return: Always 0
  */
 
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
