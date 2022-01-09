9c0dd4eb7a1bad84f65d666a4d6648296ad644c9
andresovichh <ahenderson.holberton@gmail.com>
2022-01-09 10:35:12 -0300

    test

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,9 +1,10 @@
 #include "lists.h"
 
 /**
- * @brief 
- * 
- * 
+ * get_dnodeint_at_index - gets node at index
+ * @head: ptr to head
+ * @index: index
+ * Return: the node
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
