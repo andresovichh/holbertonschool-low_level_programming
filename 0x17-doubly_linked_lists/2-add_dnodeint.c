4c0e2a6d458059167ed4280f5026ea38b7962ab9
andresovichh <ahenderson.holberton@gmail.com>
2022-01-09 09:09:26 -0300

    test

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -7,16 +7,22 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-dlistint_t *i = head;
-i->n = n;
+dlistint_t *i;/** create the new node*/
 
-i->next = (*head);
-i->prev = NULL;
+i = malloc(sizeof(dlistint_t));/**allocate mem*/
+if (i == NULL)
+{
+return (NULL);
+}
+i->n = n;/** insert data to the newly created node*/
+
+i->next = (*head);/** point next ptr to head*/
+i->prev = NULL;/** point prev ptr to NULL*/
 if ((*head) != NULL)
 {
 (*head)->prev = i;
 }
 (*head) = i;
 
-return ()
+return (i);
 }
