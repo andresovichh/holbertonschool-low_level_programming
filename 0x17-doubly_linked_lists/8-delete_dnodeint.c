b23c91d5e82052dc113aea966123b813e3d1afd1
andresovichh <ahenderson.holberton@gmail.com>
2022-01-09 22:43:30 -0300

    test

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -10,12 +10,19 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 dlistint_t *tmp = *head;
 
-if (*head == NULL || int == NULL)
+if (*head == NULL)
 return (-1);
-for (; index != 0, index--)
+for (; index != 0; index--)
 {
 if (tmp == NULL)
 return (-1);
 tmp = tmp->next;
 }
 if (tmp == *head)
+{
+*head = tmp->next;
+if (*head != NULL)
+(*head)->prev = NULL;
+}
+return (1);
+}
