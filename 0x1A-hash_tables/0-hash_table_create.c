8ed4c41a97247b2d1accad7d4fba65fd9d8c9d3f
andresovichh <ahenderson.holberton@gmail.com>
2022-01-27 19:20:43 -0300

    test

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Creates a hash table
+ * @size: the size of the array
+ * Return: The hash_table_t struct or null
+ *
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+/** 1. create the instance of the table */
+
+hash_table_t *a_table;
+
+/** 2. try to allocate memory */
+
+a_table = malloc(sizeof(hash_node_t));
+if (!a_table)
+return (NULL);
+
+a_table->size = size;
+/** malloc the hash_node_t array struct */
+
+a_table->array = malloc(sizeof(hash_table_t) * size);
+if (!a_table->array)
+return (NULL);
+
+return (a_table);
+}
