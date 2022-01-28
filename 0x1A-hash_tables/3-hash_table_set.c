70b5602ca13af5e676d6c1456fcea6940ca8914f
andresovichh <ahenderson.holberton@gmail.com>
2022-01-28 20:30:06 -0300

    test

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,37 +10,18 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-<<<<<<< HEAD
 hash_node_t *a_node;
 char *value_copy;
 unsigned long int index, i;
-=======
-hash_node_t *a_node, *tmp;
-unsigned int index;
->>>>>>> 0718521fd5ae120c3c1db151db07654b12fcc223
 
-index = key_index((const unsigned char *) key, ht->size);
-
-if (ht == NULL)
-{
+if (ht == NULL || key == NULL || value == NULL)
 return (0);
-}
 
-<<<<<<< HEAD
 /** try to allocate mem to a node, exit if fail */
 a_node = malloc(sizeof(hash_node_t));
 if (!a_node)
-=======
-a_node =  malloc(sizeof(hash_node_t));
-if (!a_node)
-{
->>>>>>> 0718521fd5ae120c3c1db151db07654b12fcc223
 return (0);
-}
-a_node->key = strdup(key);
-a_node->value = strdup(value);
 
-<<<<<<< HEAD
 a_node->key = strdup(key);
 a_node->value = strdup(value);
 
@@ -54,20 +35,10 @@ node_handler(ht, a_node);
  * node_handler - handles a node
  * @ht: the hash table of the lists
  * @node: the linked list to add or update 
-=======
-node_handler(ht, a_node);
-return (0);
-}
-
-/**
- * @brief 
- * 
->>>>>>> 0718521fd5ae120c3c1db151db07654b12fcc223
  */
 
 void node_handler(hash_table_t *ht, hash_node_t *node)
 {
-<<<<<<< HEAD
 /** getting the index & creating a *tmp */
 unsigned long int i = key_index(node->key, ht->size);
 hash_node_t *tmp = ht->array[i];
@@ -78,23 +49,12 @@ if (ht->array[i] != NULL)
 tmp = ht->array[i];
 while(tmp != NULL) /** walk the nodes*/
 {/** if there is a coincidence */
-=======
-unsigned long int i = key_index(node->key, ht->size);
-hash_node_t *tmp = ht->array[i];
-
-if (ht->array[i] != NULL)
-{
-tmp = ht->array[i];
-while (tmp != NULL)
-{
->>>>>>> 0718521fd5ae120c3c1db151db07654b12fcc223
 if (strcmp(tmp->key, node->key) == 0)
 {
 break;
 }
 tmp = tmp->next;
 }
-<<<<<<< HEAD
 if (tmp == NULL) /** if at last node*/
 {
 node->next = ht->array[i];
@@ -107,20 +67,6 @@ tmp->value = strdup(node->value);
 free(node->value);
 free(node->key);
 free(node);
-=======
-if (tmp == NULL)
-{
-node->next = ht->array[i];
-ht->array[i] =  node;
-}
-else
-{
-  free(tmp->value);
-  tmp->value = strdup(node->value);
-  free(node->value);
-  free(node->key);
-  free(node);
->>>>>>> 0718521fd5ae120c3c1db151db07654b12fcc223
 }
 }
 else
@@ -128,8 +74,4 @@ else
 node->next = NULL;
 ht->array[i] = node;
 }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 0718521fd5ae120c3c1db151db07654b12fcc223
