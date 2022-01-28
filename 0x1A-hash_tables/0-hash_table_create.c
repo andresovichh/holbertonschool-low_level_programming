3671f3744aa038b1e0675a6071aa03cf8279d2cb
andresovichh <ahenderson.holberton@gmail.com>
2022-01-28 10:07:29 -0300

    test

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,22 +10,24 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 /** 1. create the instance of the table */
 hash_table_t *a_table;
+unsigned int i = 0;
+
 /** 2. try to allocate memory */
 a_table = malloc(sizeof(hash_node_t));
 if (!a_table)
 {
-return (NULL);
 free (a_table);
+return (NULL);
 }
 a_table->size = size;
 /** malloc the hash_node_t array struct */
-a_table->array = malloc(sizeof(hash_table_t) * size);
+a_table->array = malloc(sizeof(hash_table_t *) * size);
 if (!a_table->array)
 {
+free (a_table->array);
 return (NULL);
-free (a_table->array);
 }
-free (a_table);
-free (a_table->array);
+for (i = 0; i < size; i++)
+a_table->array[i] = NULL;
 return (a_table);
 }
