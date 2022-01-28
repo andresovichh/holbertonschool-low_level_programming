ccc9395fd740d6ff7019d33decb106122d7323b5
andresovichh <ahenderson.holberton@gmail.com>
2022-01-28 11:41:00 -0300

    test

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -12,6 +12,10 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 unsigned long int i = 0, hash = 0;
 
+if (key == NULL)
+return (0);
+if (size == 0)
+return (0);
 while (key && key[i])
 {
 hash = (hash + key[i]) % size;
