70862bf8177b6a85150d8c0829cbcbd267f89c97
andresovichh <ahenderson.holberton@gmail.com>
2022-01-28 10:31:30 -0300

    correct sizeof

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,25 +7,19 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-/** 1. create the instance of the table */
 hash_table_t *a_table;
 unsigned long int i = 0;
 
-if (!size)
-return (NULL);
 if (size == 0)
 return (NULL);
 
-/** 2. try to allocate memory */
-a_table = malloc(sizeof(hash_node_t));
+a_table = malloc(sizeof(hash_table_t));
 if (a_table == NULL)
 {
 free(a_table);
 return (NULL);
 }
-
-/** malloc the hash_node_t array struct */
-a_table->array = malloc(sizeof(hash_table_t *) * size);
+a_table->array = malloc(sizeof(hash_node_t *) * size);
 if (a_table->array == NULL)
 {
 free(a_table->array);
