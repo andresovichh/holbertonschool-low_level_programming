c61545b9ec99fc8496324ee43be86a2c0afe0a16
andresovichh <ahenderson.holberton@gmail.com>
2022-01-28 11:36:57 -0300

    test

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,21 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - Gets the index of a key
+ *
+ * @key: the key
+ * @size: the size of the array of the hash table
+ * Return: Always 0
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+unsigned long int i = 0, hash = 0;
+
+while (key && key[i])
+{
+hash = (hash + key[i]) % size;
+i++;
+}
+return (hash);
+}
