634d545e2ccd62d413ba6cf142ebaca9b2f4dd48
andresovichh <ahenderson.holberton@gmail.com>
2022-01-28 23:12:19 -0300

    test

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,40 @@
+#include "hash_tables.h"
+
+
+/**
+ * hash_table_delete - free a hash table
+ * @ht: the hash table to delete
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+hash_node_t *tmp, *node;
+unsigned long int idx = 0;
+
+
+if (!ht)
+return;
+
+if (!(ht->array))
+{ /** if no array, free and return */
+free(ht);
+return;
+}
+
+for (idx = 0; idx < ht->size; idx++)
+{
+node = (ht->array)[idx];
+while (node) /** while nodes exist in the array */
+{
+tmp = node->next; /** set to next */
+if (node->key) /** if key exists, free */
+free(node->key);
+if (node->value) /** if value exists, free */
+free(node->value);
+/** node->key = NULL; */
+/**node->value = NULL; */
+free(node);
+node = tmp;
+}
+}
+
+}
