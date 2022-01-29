1ed731a91f8a115a7aa30f837b45b91337d53685
andresovichh <ahenderson.holberton@gmail.com>
2022-01-28 21:50:23 -0300

    test

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * @brief 
+ * 
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+hash_node_t *tmp; /** a tmp to traverse */
+unsigned long int i = 0;
+
+/** traverse the nodes */
+while (i < ht->size)
+{
+tmp = ht->array[i];
+{
+while (tmp) /** walk untill null */
+{
+printf("%s", tmp->key);
+/** check if next is null */
+if (ht->array[i +1] == NULL)
+break;
+
+tmp = tmp->next;
+}
+i++;
+}
+}
+}
